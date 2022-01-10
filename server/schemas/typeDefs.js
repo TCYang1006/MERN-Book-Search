19c37d4b1f3e11a2f829aa2eea55dade02c45653
Peter Thao <peterthao@Peters-iMac.local>
2022-01-10 17:41:55 -0500

    typeDefs complete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,17 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(author)
+    }
+
     type User {
         _id: ID
         username: String
@@ -16,16 +27,6 @@ const typeDefs = gql`
         title: String
         image: String
         link: String
-    }
-
-    type Query {
-        me: User
-    }
-
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(author)
     }
 
     type Auth {
