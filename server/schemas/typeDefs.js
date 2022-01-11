f580057b744b549f172f1743747aaeee42bfc834
Peter Thao <peterthao@Peters-iMac.local>
2022-01-10 22:51:12 -0500

    started client side

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,8 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(author)
+        saveBook(bookData: BookInput!):User
+        removeBook(bookID: String):User
     }
 
     type User {
