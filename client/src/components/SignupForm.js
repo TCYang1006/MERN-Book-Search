fbfc90c24a042cf082634a6b264c953dc6d14591
Peter Thao <peterthao@Peters-iMac.local>
2022-01-11 23:47:52 -0500

    signup form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 
-import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  // set state for form validation
+  const [userFormData, setUserFormData] = useState({ 
+    username: '',
+    email: '',
+    password: ''
+  });
   const [validated] = useState(false);
-  // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    setUserFormData({ 
+      ...userFormData, 
+      [name]: value
+    });
   };
 
   const handleFormSubmit = async (event) => {
@@ -28,17 +36,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
+      const response = await addUser(userFormData);
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
-
       const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
-    } catch (err) {
-      console.error(err);
+    } catch (e) {
+      console.error(e);
       setShowAlert(true);
     }
 
